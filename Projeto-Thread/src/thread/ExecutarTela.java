2d85f7666fd20ef76c8dd3c2ed330e5a75da7cdd
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-05-25 23:11:48 -0300

    criando pilha e fila para processo paralelo em Thread e fazendo Implementaçao do processo de fila e pilha com Thread

--- Projeto-Thread/src/thread/ExecutarTela.java
+++ Projeto-Thread/src/thread/ExecutarTela.java
@@ -2,4 +2,7 @@ package thread;
 
 public class ExecutarTela {
 
+	public static void main(String[] args) {
+		TelaTimeThread telaTimeThread = new TelaTimeThread();
+	}
 }
---
+++ Projeto-Thread/src/thread/ImplementacaoFilaThread.java
@@ -0,0 +1,63 @@
+package thread;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class ImplementacaoFilaThread extends Thread{
+
+	//metodo statico para acessar essa pilha fila 
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_fila = 
+			new ConcurrentLinkedQueue<ObjetoFilaThread>();
+	
+	
+	public static void add(ObjetoFilaThread objetoFilaThread) {
+		
+		pilha_fila.add(objetoFilaThread);
+	}
+	
+	@Override
+	public void run() {
+		
+		System.out.println("fila rodando");
+		
+		
+		
+		while(true) {
+			java.util.Iterator iteracao = pilha_fila.iterator();//Intereagindo com a lista
+			
+		synchronized (iteracao) {//Bloqueando o acesso a essa lista para outro processo 
+			
+			while (iteracao.hasNext()) {//Enquanto tiver dados na lista vai processar
+				
+				ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next();//pegando o objeto atual
+				
+				//processar 20 mil notas FISCAL
+				//Gera uma lista enorme de PDF
+				//gera um envio em massa de E-MAIL
+				
+				//testando
+				System.out.println("----------------------------");
+				System.out.println(processar.getEmail());
+				System.out.println(processar.getNome());
+				
+				iteracao.remove();//Terminou removendo o objeto
+				
+				try {
+					Thread.sleep(1000);//Da um tempo para a carga de memoria
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+		
+		try {
+			Thread.sleep(1000);//Dar um tempo para a limpesa de memopria 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		}
+	}
+}
